38c337d0d3391239b30e43834e2e1ade2a0f7f62
Matheus Vinicius França <matheusviniciusfranca@Matheuss-MacBook-Air.local>
2024-04-14 18:33:29 -0300

    metodo para calcular imc feito

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,15 +1,30 @@
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 import { BMIInput } from "../../components/BMIInput";
+import { useState } from "react";
 
 export function Home() {
+  const [bmiValue, setBmiValue] = useState(0);
+
+  function calculateBMI(weight, height) {
+    var floatWeight = parseFloat(weight.replace(",", "."));
+    console.log(floatWeight);
+    var floatHeight = parseFloat(height.replace(",", "."));
+    console.log(floatHeight);
+
+    setBmiValue(floatWeight / (floatHeight * floatHeight));
+    console.log("calculate BMI: " + bmiValue);
+  }
+
   return (
     <View>
       <View style={styles.titleContainer}>
         <Text style={styles.titleText}>Calculadora - IMC</Text>
         <View style={styles.titleLineStyle}></View>
       </View>
-      <BMIInput></BMIInput>
+      <BMIInput onClickButton={calculateBMI} />
     </View>
   );
 }
+
+//export default Home;
--- src/components/BMIInput/index.tsx
+++ src/components/BMIInput/index.tsx
@@ -1,8 +1,12 @@
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { styles } from "./styles";
 import { theme } from "../../theme";
+import { useState } from "react";
 
-export function BMIInput() {
+export function BMIInput(props) {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+
   return (
     <View style={styles.inputContainer}>
       <Text style={styles.inputHeaderText}>Peso em kg</Text>
@@ -10,15 +14,23 @@ export function BMIInput() {
         style={styles.textInputContainer}
         keyboardType="numeric"
         placeholder="Kg"
+        onChangeText={setWeight}
+        value={weight}
       ></TextInput>
       <Text style={styles.inputHeaderText}>Altura em metros</Text>
       <TextInput
         style={styles.textInputContainer}
         keyboardType="numeric"
         placeholder="Metros"
+        onChangeText={setHeight}
+        value={height}
       ></TextInput>
       <View style={styles.confirmButton}>
-        <Button title="Calcular" color={theme.colors.greenCeltics} />
+        <Button
+          title="Calcular"
+          color={theme.colors.greenCeltics}
+          onPress={props.onClickButton.bind(this, weight, height)}
+        />
       </View>
     </View>
   );
